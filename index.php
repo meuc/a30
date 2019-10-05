84e50a5116a2800cad3e9f7d5f9b9d9f21f18f7f
Marie Crabit <mar.rie.cra@gmail.com>
2019-10-05 18:14:04 +0200

    php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" href="favicon.ico" />	
+		<link rel="stylesheet" href="css/normalize.css"> 
+		<link rel="stylesheet" href="css/style.css"> 
+		<title>A30 Slides</title>
+	</head>
+
+	<body>
+		
+		
+		
+		<h1>Alpha 3.0</h1>
+		<h2>présentations</h2>
+		
+		
+		<div class="content">
+				<!-- <a href="/slides/test.png" class="item">foo</a> -->
+				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
+				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
+				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
+				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
+				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
+				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
+				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
+		</div>
+		
+		
+		
+		<script src="js/maxibear.js"></script>
+		<script src="js/jquery-2.1.1.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+
+</html>	
