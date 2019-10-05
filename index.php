4c1174de228b2d6733b499620fa86ef71a4dc9fb
Marie Crabit <mar.rie.cra@gmail.com>
2019-10-05 18:37:05 +0200

    hardcode content

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="shortcut icon" href="favicon.ico" />	
+		<!-- <link rel="shortcut icon" href="favicon.ico" />	 -->
 		<link rel="stylesheet" href="css/normalize.css"> 
 		<link rel="stylesheet" href="css/style.css"> 
 		<title>A30 Slides</title>
@@ -21,13 +21,23 @@
 		
 		<div class="content">
 				<!-- <a href="/slides/test.png" class="item">foo</a> -->
-				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
-				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
-				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
-				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
-				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
-				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
-				<a href="https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg" target="_blank" class="item">foo</a>
+				<a href="slides/CIBE 01-02 Hygiène en campagne.pptx" target="_blank" class="item">CIBE 01-02</a>
+				<a href="slides/CIBE 03-06 Protocole d'approche.pptx" target="_blank" class="item">CIBE 03-06</a>
+				<a href="slides/CIBE 07-08 Shock.pptx" target="_blank" class="item">CIBE 07-08</a>
+				<a href="slides/CIBE 09-10 Hémorragies.pptx" target="_blank" class="item">CIBE 09-10</a>
+				<a href="slides/KALI-01-2019-VNL.pptx" target="_blank" class="item">KALI 01</a> 
+				<a href="slides/KALI-02-2019-VNL.pptx" target="_blank" class="item">KALI 02</a> 
+				<a href="slides/KALI-03-2019-VNL.pptx" target="_blank" class="item">KALI 03</a> 
+				<a href="slides/KALI-04-2019-VNL.pptx" target="_blank" class="item">KALI 04</a> 
+				<a href="slides/NBCI 06 Le danger radiologique.pptx" target="_blank" class="item">NBCI 06</a>
+				<a href="slides/REGI 05-06 AMS, cont med, hosp.pptx" target="_blank" class="item">REGI 05-06</a>
+				<a href="slides/REGI 07 Les grades.pptx" target="_blank" class="item">REGI 07</a>
+				<a href="slides/REGI 08 Désertion.pptx" target="_blank" class="item">REGI 08</a>
+				<a href="slides/REGI 09 La discipline militaire.pptx" target="_blank" class="item">REGI 09</a>
+				<a href="slides/REGI 10 Droits et devoirs.pptx" target="_blank" class="item">REGI 10</a>
+				<a href="slides/REGI 11  La procédure disciplinaire.pptx" target="_blank" class="item">REGI 11</a>
+				<a href="slides/REGI 12  Congé dispense de service.pptx" target="_blank" class="item">REGI 12</a>
+				<!-- <a href="slides/" target="_blank" class="item">foo</a> -->
 		</div>
 		
 		
